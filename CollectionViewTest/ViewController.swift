4fbb1569f7f60e91a90ea577b7c715ffc79254df
Fidel Cornelis <fidel.cornelis@gmail.com>
2015-10-29 20:58:20 +0100

    Initial Commit

---
+++ CollectionViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by belga on 29/10/15.
+//  Copyright © 2015 Fidel Cornelis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
